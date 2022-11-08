c7eeeca2c3ac901b059bc93d3c2f6fe2b098603b
jeeminhan <jeeminjameshan@gmail.com>
2022-11-08 01:25:40 -0600

    Added twitter API functionality to get Elon Musks top ten tweets

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 import dotenv
 import os
 import requests
+import tweepy
 from pathlib import Path
 
 # Create your views here.
@@ -11,28 +12,53 @@ dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
         dotenv.load_dotenv(dotenv_file)
 NEWS_API_KEY = os.environ['NEWS_API_KEY']
+BEARER_TOKEN = os.environ['BEARER_TOKEN']
+
+# def index(request):
+#     # API function
+#     news_titles=news_API()
+
+#     # Data transformation for display
+#     response={"news":news_titles}
+
+#     return render(request, "index.html", response)
+
+# # HELPER FUNCTIONS TO PERFORM AND PROCESS THE API requests
+# def news_API():
+
+#     # Link for making request
+#     url="https://newsapi.org/v2/top-headlines?country=us&apiKey="+NEWS_API_KEY
+
+#     news=requests.get(url).json()
+
+#     # Storing the top headings
+#     titles=[]
+
+#     for i in range(5):
+#         titles+=[news['articles'][i]['title']]
+    
+#     return titles
 
 def index(request):
     # API function
-    news_titles=news_API()
+    tweets=twitter_API()
 
     # Data transformation for display
-    response={"news":news_titles}
+    response={"tweets":tweets}
 
     return render(request, "index.html", response)
 
-# HELPER FUNCTIONS TO PERFORM AND PROCESS THE API requests
-def news_API():
 
+def twitter_API():
     # Link for making request
-    url="https://newsapi.org/v2/top-headlines?country=us&apiKey="+NEWS_API_KEY
+    client = tweepy.Client(bearer_token=BEARER_TOKEN)
+    query = 'from:elonmusk -is:retweet'
 
-    news=requests.get(url).json()
+    tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=10)
 
-    # Storing the top headings
-    titles=[]
+    tweets_data=[]
 
-    for i in range(5):
-        titles+=[news['articles'][i]['title']]
+    for tweet in tweets.data:
+        tweets_data.append(tweet.text)
     
-    return titles
+    return tweets_data
