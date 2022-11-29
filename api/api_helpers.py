95882cb014800d947ee353becfc961b11f00918f
akash-gajendra <akash.gajendra@tamu.edu>
2022-11-28 19:48:44 -0600

    Updated api.helper.py

--- api/api_helpers.py
+++ api/api_helpers.py
@@ -5,6 +5,13 @@ import requests
 import tweepy
 from pathlib import Path
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import datetime
+import os.path
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
@@ -68,3 +75,64 @@ def weather_API():
     results=[weather['weather'][0]['main'],str(weather['main']['humidity']),str(int(weather['main']['temp'])-273)+"\xb0"+"C", 'http://openweathermap.org/img/w/'+weather['weather'][0]['icon']+'.png']
 
     return results
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def googleCal_API():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '../credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    # try:
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    # print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                            maxResults=10, singleEvents=True,
+                                            orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    # page_token = None
+    # while True:
+    #     events = service.events().list(calendarId='primary', pageToken=page_token).execute()
+    #     for event in events['items']:
+    #         print(event['summary'])
+    #     page_token = events.get('nextPageToken')
+    #     if not page_token:
+    #         break
+
+    if not events:
+        # print('No upcoming events found.')
+        return
+
+    # Prints the start and name of the next 10 events
+    calList = []
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        end = event['end'].get('dateTime', event['end'].get('date'))
+        calList.append({'date':start[0:10], 'startTime':start[11:19], 'endTime':end[11:19], 'summary':event['summary']})
+        # return (start, end, event['summary'])
+    return calList
+
+    # except HttpError as error:
+    #     return ('An error occurred: %s' % error)
